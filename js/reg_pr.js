9c969b6b70f1a6748680b231d9a2647ac1f98999
ccamargo8 <-ingcarloscamargo87@hotmail.com->
2021-09-30 20:44:39 -0500

    Icons and JS
    Updated JS and added icons to buttons

--- js/reg_pr.js
+++ js/reg_pr.js
@@ -3,3 +3,17 @@ document.getElementById("Registrar").onclick = muestraMensaje;
 function muestraMensaje() {
     alert('Se ha registrado el producto exitosamente');
   }
+
+  const  generateRandomString = (num) => {
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result1= ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < num; i++ ) {
+        result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result1;
+    
+}
+document.getElementById("id").innerHTML = result1;
+document.getElementById('id').addEventListener('click', generateRandomString);
