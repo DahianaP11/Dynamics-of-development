891bf370483ecbd52a1c4518f0e3e55003171b14
ccamargo8 <-ingcarloscamargo87@hotmail.com->
2021-10-06 22:20:45 -0500

    Tabla y cambios
    Se agregó una tabla en la que se espera ir agregando los productos y que se muestre los 3 ultimos agregados. Se agregó al JS la función de asignar automaticamente el id del producto pero no logra funcionar.

--- js/reg_pr.js
+++ js/reg_pr.js
@@ -1,17 +1,29 @@
+
+
 document.getElementById("Registrar").onclick = muestraMensaje;
 
 function muestraMensaje() {
     alert('Se ha registrado el producto exitosamente');
   }
 
-function makeid(length) {
+function makeid(){   
    var result           = '';
    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
    var charactersLength = characters.length;
-   for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   for ( var i = 0; i < 9; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));      
    }
-   return result;
+      
+      
+   document.getElementById("identificadorP").readonly = false;   
+   document.getElementById('identificadorP').value=result;
+   document.getElementById("identificadorP").readonly = true;
+   
+   
+   
+   
+   // return result;
 }
-document.getElementById("id").innerHTML = result;
-document.getElementById('id').addEventListener('change', generateRandomString);
+
+
+
