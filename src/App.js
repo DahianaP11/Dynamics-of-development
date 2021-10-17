ee2cd861bc422122468a2ac77f2463f768b6a171
Legnatbird <50637445+WholeMuffin@users.noreply.github.com>
2021-10-17 15:27:33 -0500

    Se agrega el maestro de usuarios
    Falta hacer ajustes de la ventana modal para actualizar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Inicio from "./components/Inicio";
 import MaestroProductos from "./components/MaestroProductos";
+import MaestroUsuario from "./components/MaestroUsuario";
 import MaestroVentas from "./components/MaestroVentas";
 import RegProductos from "./components/RegProductos";
 import RegVentas from "./components/RegVentas";
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <section>
-        <RegVentas />
+        <MaestroUsuario />
       </section>
     </div>
   );
