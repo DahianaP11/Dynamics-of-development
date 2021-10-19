0a477404f97dbe0b83382581ce8eb656af3c73ab
Legnatbird <50637445+WholeMuffin@users.noreply.github.com>
2021-10-19 11:55:52 -0500

    Se agregan estilos básicos

--- src/components/Foooter.js
+++ src/components/Foooter.js
@@ -2,7 +2,7 @@ import logo from "../img/logo_DOD.png";
 
 export default function Footer(){
     return(
-        <footer>
+        <footer className="footer">
             <img src={logo} width="60px"/>
             <p>Contactanos: ventasdod@dod.com</p>
         </footer>
