7fe08a2038c4d34246047747a497c5d1f74606ce
DahianaP11 <dahiana.p11@hotmail.com>
2021-10-28 22:11:25 -0500

    Se crean carpetas de Fronted y Backend y además se agregan recursos necesarios por el Fronted

---
+++ fronted/src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Inicio from "./components/Inicio";
+import MaestroProductos from "./components/MaestroProductos";
+import MaestroUsuario from "./components/MaestroUsuario";
+import MaestroVentas from "./components/MaestroVentas";
+import RegProductos from "./components/RegProductos";
+import RegVentas from "./components/RegVentas";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Foooter";
+import "./AppCss.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Inicio} />
+          <Route path="/Registro_Venta" exact component={RegVentas} />
+          <Route path="/Maestro_Ventas" exact component={MaestroVentas} />
+          <Route path="/Registro_Productos" exact component={RegProductos} />
+          <Route path="/Maestro_Productos" exact component={MaestroProductos} />
+          <Route path="/Maestro_Usuario" exact component={MaestroUsuario} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
